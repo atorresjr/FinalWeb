fcf3e13a5cddccb5a9855ef8396ce2d538ebfd5f
Alfonso Torres <atorresjr1@hotmail.com>
2021-05-09 19:00:59 -0500

    Added Detail

--- src/components/Computer.vue
+++ src/components/Computer.vue
@@ -3,7 +3,7 @@
     <div class="bg-yellow-400 text-yellow-800 rounded text-center p-2 space-y-2">
         <h3 class="text-2xl font-thin tracking-wide">Computer Brand</h3>
         <p class="text-sm font-medium text-yellow-900">Computer Model Will Go Here</p>
-        <div class="flex justify-end space-x-4">
+        <div class="flex justify-end space-x-5">
             <button class="hover:text-green-800">
                 <flat-color-icons:add-database />
             </button>
--- src/views/Computers.vue
+++ src/views/Computers.vue
@@ -4,6 +4,7 @@
   >
     <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 container mx-auto">
       <Computer v-for="n in 10" :key="n" />
+      <InventoryAdd />
     </div>
   </div>
 </template>
