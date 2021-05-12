e0d774df1b1e91c44677da122086d6b967f34a87
Alfonso Torres <atorresjr1@hotmail.com>
2021-05-11 23:43:25 -0500

    Final sub

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB8iU0E4mDR75MBIidssdjn9nX3Cn7wjo8",
+    authDomain: "finalprojectauthen.firebaseapp.com",
+    projectId: "finalprojectauthen",
+    storageBucket: "finalprojectauthen.appspot.com",
+    messagingSenderId: "728957051657",
+    appId: "1:728957051657:web:28f5c6ea1e74fd471529d8"
+}
